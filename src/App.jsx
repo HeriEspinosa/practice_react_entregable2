108c3e201098bbfbf5630f0076896269937ecec9
HeriEspinosa <espinosa_reyes@hotmail.com>
2023-01-31 14:13:50 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Loading from './Components/Loading';
 
 function App() {
   const [coords, setCoords] = useState();
-  const [weather,SetWeather] = useState();
+  const [weather, SetWeather] = useState();
   const [temperature, setTemperature] = useState();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-  
+
     const success = pos => {
       const obj = {
         lat: pos.coords.latitude,
@@ -22,8 +22,8 @@ function App() {
     navigator.geolocation.getCurrentPosition(success)
   }, [])
 
-  useEffect(()=> {
-    if(coords){
+  useEffect(() => {
+    if (coords) {
       const APIkey = 'f450c4c1589803de11bb22bc100d906b'
       const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&APPID=${APIkey}`
 
@@ -32,29 +32,29 @@ function App() {
           SetWeather(res.data)
           const obj = {
             celsius: (res.data.main.temp - 273.15).toFixed(1),
-            farenheit: ((res.data.main.temp - 273.15) * 9/5 + 32).toFixed(1)
+            farenheit: ((res.data.main.temp - 273.15) * 9 / 5 + 32).toFixed(1)
           }
           setTemperature(obj)
         })
-        
+
         .catch(err => console.log(err))
-        .finally(()=> setIsLoading(false))
+        .finally(() => setIsLoading(false))
     }
-  },[coords])
+  }, [coords])
 
   return (
     <div className="App">
       {
-        isLoading ? 
-        <Loading/>
-        :
-        <WeatherCard 
-          weather={weather}
-          temperature={temperature}
-      />
+        isLoading ?
+          <Loading />
+          :
+          <WeatherCard
+            weather={weather}
+            temperature={temperature}
+          />
       }
       <div className='hr__code'>
-        <a href="https://github.com/HeriEspinosa/repo_react_entregable2.git"><p>Hecha un Vistazo a mi Codigo <span>Aqui</span></p></a>
+        <a href="https://github.com/HeriEspinosa/repo_react_entregable2.git" target='blank'><p>Hecha un Vistazo a mi Codigo <span>Aqui</span></p></a>
       </div>
     </div>
   )
